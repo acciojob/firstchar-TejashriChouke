2d69615964b344c022efd3ce818075cc78bafaeb
Tejashri Rajendra Chouke <139746868+TejashriChouke@users.noreply.github.com>
2024-04-05 23:39:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function firstChar(text) {
   // your code here
+	 for (let char of text) {
+        if (char !== ' ') {
+            return char;
+        }
+    }
+    return '';
+	
 }
+
 
 // Do not change the code below
 
